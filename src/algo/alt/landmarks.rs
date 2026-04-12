30c601e5a56e769bcbd06ff7e0567cb70ff90ae1
akwasniewski <andrzej@akwasniewski.eu>
2026-04-12 23:13:34 +0200

    farthest alt

--- src/algo/alt/landmarks.rs
+++ src/algo/alt/landmarks.rs
@@ -1,17 +1,21 @@
+use std::{collections::HashMap, hash::Hash};
+
+use eframe::egui::Order;
 use ordered_float::OrderedFloat;
 
 use crate::graph::{Graph, LandmarkData};
 impl Graph {
     pub fn get_random_landmarks(&mut self, count: usize) {
+        self.landmarks = HashMap::new();
         let mut i = 0;
         while i < count {
-            let cur = rand::random_range(0..=self.size);
+            let cur = rand::random_range(0..self.size);
 
             if self.landmarks.contains_key(&cur) {
                 continue;
             }
 
-            self.landmarks.insert(
+        self.landmarks.insert(
                 cur,
                 LandmarkData {
                     to: self.distance_to_all(cur),
@@ -20,6 +24,35 @@ impl Graph {
             );
             i += 1;
         }
+    }
+
+    pub fn get_farthest_landmarks(&mut self, count: usize){
+        self.landmarks = HashMap::new();
+        let mut min_distances = vec![OrderedFloat(f64::MAX); self.size];
+
+        let mut cur = rand::random_range(0..self.size); 
+        for _ in 0..count{
+            self.get_landmark_data(cur);
+            let cur_distances: &Vec<OrderedFloat<f64>> = self.landmarks.get(&cur).as_ref().unwrap().from.as_ref();
+            let mut next: (OrderedFloat<f64>, usize) = (OrderedFloat(0.0), 0);
+            for j in 0..self.size{
+                min_distances[j]=cur_distances[j].min(min_distances[j]);
+                if min_distances[j]>next.0 && min_distances[j]!=OrderedFloat(f64::MAX){
+                    next = (min_distances[j], j);
+                }
+            }
+            cur = next.1;
+        }
+    }
+
+    pub fn get_landmark_data(&mut self, vertex: usize){
+        self.landmarks.insert(
+                vertex,
+                LandmarkData {
+                    from: self.distance_to_all(vertex),
+                    to: self.distance_to_all_rev(vertex),
+                },
+            );
     }
 }
 
